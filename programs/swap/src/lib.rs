0e207cc2b5ef25b7767b5a0d6df0096c4bd9e52e
TaimoorKhan101 <3tk2021@gmail.com>
2025-04-20 06:02:30 -0700

    Implemented the Smart Contract for the SwapProgram + Debugged the IDL issue

--- programs/swap/src/lib.rs
+++ programs/swap/src/lib.rs
@@ -8,14 +8,22 @@ use anchor_lang::prelude::*;
 pub use constants::*;
 pub use instructions::*;
 pub use state::*;
-
 declare_id!("GGxQgKx7LPTX2vU4E5VmmvKerJJCq9VZQewYH4zmqVE6");
 
 #[program]
 pub mod swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        ctx: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault
+        (&ctx, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(ctx, id,
+        token_b_wanted_amount);  
+        Ok(())      
     }
 }
